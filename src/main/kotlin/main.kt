9794fbb21a4854457083f45730a3d3200030f6f5
seva-vaskin <vaskin.2003@gmail.com>
2021-09-15 00:05:37 +0300

    created basic checks for command line arguments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,35 @@
+import java.io.File
+import kotlin.system.exitProcess
+
+/**
+ * check if file is correct for diff utility
+ */
+fun checkFile(file: File): Boolean {
+    if (!file.isFile) {
+        System.err.println("${file.name}: No such file or directory")
+        return false
+    }
+    if (!file.canRead()) {
+        System.err.println("${file.name}: Can't read this file")
+        return false
+    }
+    return true
+}
+
 fun main(args: Array<String>) {
-    TODO()
+    if ("--help" in args) {
+        println("Help message")
+        TODO("print help message here")
+    }
+    if (args.size < 2) {
+        System.err.println("Try 'diff --help' for more information.")
+        exitProcess(1)
+    }
+    val firstFileName: String = args[0]
+    val secondFileName: String = args[0]
+    val firstFile = File(firstFileName)
+    val secondFile = File(secondFileName)
+    if (!checkFile(firstFile) || !checkFile(secondFile)) {
+        exitProcess(1)
+    }
 }
